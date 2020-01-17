f7fb8e6ad9189e59b26d6c81bac2195a40df587b
Jonathan Dorety <kyullog@gmail.com>
2020-01-16 22:02:58 -0800

    Fix post sorting order

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default ({ data }) => (
 )
 export const pageQuery = graphql`
   query MyQuery {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: ASC }) {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
       edges {
         node {
           id
