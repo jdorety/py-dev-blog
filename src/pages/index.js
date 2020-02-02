ce01f6c226d9f5d595701e14645625da76fd950d
Jonathan Dorety <kyullog@gmail.com>
2020-02-01 21:58:25 -0800

    Major restyling for main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,37 @@ import React from "react"
 import { graphql } from "gatsby"
 import BlogLink from "../components/blogLink"
 import Layout from "../components/layout/layout"
+import PythonLogo from "../assets/python-original.svg"
+import PiLogo from "../assets/raspberry-pi.svg"
 import "./index.scss"
 
 export default ({ data }) => (
   <Layout>
-    <h1>Welocome to the PyClock2 Development Blog</h1>
+    <div id="site-header">
+      <h1>Welcome to the PyClock2 Development Blog</h1>
     <p>
       This is a blog for the project that really pushed me into the world of
       software development
     </p>
-    {data.allMarkdownRemark.edges.map(post => {
-      return (
-        <BlogLink
-          path={post.node.frontmatter.path}
-          key={post.node.id}
-          title={post.node.frontmatter.title}
-          author={post.node.frontmatter.author}
-        />
-      )
-    })}
+    </div>
+    <div className="post-list-wrapper">
+      <div className="blog-column">
+        {data.allMarkdownRemark.edges.map(post => {
+          return (
+            <BlogLink
+              path={post.node.frontmatter.path}
+              key={post.node.id}
+              title={post.node.frontmatter.title}
+              author={post.node.frontmatter.author}
+            />
+          )
+        })}
+      </div>
+      <div className="tech-logos">
+        <img src={PythonLogo} alt="Python Logo" />
+        <img src={PiLogo} alt="Raspberry Pi logo" />
+      </div>
+    </div>
   </Layout>
 )
 export const pageQuery = graphql`
